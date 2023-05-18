fb9a9648c31bb86f004668dabef30b5777808412
webldavi <luisdavi.contato@gmail.com>
2023-05-18 03:49:15 +0000

    test

--- src/sections/Blog/Blog.tsx
+++ src/sections/Blog/Blog.tsx
@@ -1,58 +1,100 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import "./Blog.css";
 export default function Welcome() {
-    const [posts, setPosts] = useState([
-        {
+    const [posts, setPosts] = useState([{
             id: 123,
             cover_image: "",
             title: "",
             description: "",
             tag_list: [],
             canonical_url: "",
-        },
-    ]);
+            published_at: ''
+
+        },]);
+
+
+
 
     function getPostsData() {
-        fetch("https://dev.to/api/articles?username=vuedavi")
-            .then((res) => res.json())
-            .then((data) => {
-                setPosts(data);
-            });
+        fetch("https://dev.to/api/articles?username=vuedavi").then((res) => res.json()).then((data) => {
+            console.log('data', data)
+            setPosts(data);
+        });
     }
-    function redirectUser(url: string) {
+
+
+
+    function redirectUser(url : string) {
         window.open(url, "_blank");
     }
+
+    function setCardHeight() {
+        const container = document.getElementById("cardsContainer")
+        const blogCard = document.getElementById("blogCard")
+        if (container && blogCard) {
+
+            const containerHeight = document.getElementById("cardsContainer") ?. clientHeight
+            blogCard.style.height = containerHeight + "px"
+        }
+    }
+    function postDate(date : string) {
+        const dateObj = new Date(Date.parse(date));
+
+        let day: string | number = dateObj.getDate();
+        let month: string | number = dateObj.getMonth() + 1;
+        const year = dateObj.getFullYear();
+
+        day = day < 10 ? "0" + day : day
+        month = month < 10 ? "0" + month : month
+
+        const formattedDate = day + "/" + month + "/" + year;
+        return formattedDate
+
+    }
+    setCardHeight()
     useEffect(() => {
         getPostsData();
     }, []);
     return (
         <section className="blogContainer">
             <h1>#Blog</h1>
-            <div>
-                {posts.map((post) => {
+            <div id="cardsContainer">
+                {
+                posts.map((post) => {
                     return (
-                        <div key={post.id}>
-                            <img src={post.cover_image} alt="post image" />
+                        <div key={
+                                post.id
+                            }
+                            id="blogCard">
+                            <img src={
+                                    post.cover_image
+                                }
+                                alt="post image"/>
                             <div>
-                                <h1>{post.title}</h1>
-                                <p>{post.description}</p>
-                                <div>
-                                    {post.tag_list.map((tag) => {
-                                        return <div>#{tag}</div>;
-                                    })}
-                                </div>
-                                <button
-                                    onClick={() =>
-                                        redirectUser(post.canonical_url)
-                                    }
-                                >
+                                <h1>{
+                                    post.title
+                                }</h1>
+                                <p>{
+                                    post.description
+                                }</p>
+                                <div> {
+                                    post.tag_list.map((tag, index) => {
+                                        return <div key={index}>#{tag}</div>;
+                                    })
+                                } </div>
+                                <p>Publicado em {
+                                    postDate(post.published_at)
+                                }</p>
+                                <button onClick={
+                                    () => redirectUser(post.canonical_url)
+                                }>
                                     Saiba Mais
                                 </button>
                             </div>
                         </div>
                     );
-                })}
-            </div>
+                })
+            } </div>
         </section>
     );
 }
--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -1,18 +1,24 @@
 import "./Skills.css";
 import SkillsList from "./SkillsList";
 export default function Skills() {
-  return (
-    <section className="skills">
-      <h1>#Habilidades</h1>
-      <div className="SkillsContainer">
-        {SkillsList.map((s, index) => {
-          return (
-            <div>
-              <img src={s.link} key={index} alt={s.skillName} />
-            </div>
-          );
-        })}
-      </div>
-    </section>
-  );
+        return (<section className="skills">
+            <h1>#Habilidades</h1>
+            <div className="SkillsContainer">
+                {
+                SkillsList.map((s, index) => {
+                            return(< div key = {
+                                index
+                            } > <img src={
+                                    s.link
+                                }
+                                alt={
+                                    s.skillName
+                                }/>
+                        </div>
+                        );
+                    }
+                )
+            } </div>
+        </section>
+    );
 }
