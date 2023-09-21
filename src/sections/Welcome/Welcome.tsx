35699be76063a0fa30d09802e27487ec87e5f219
Luis Davi <luisdavi.contato@gmail.com>
2023-09-21 13:34:11 -0300

    initial commit

--- src/sections/Welcome/Welcome.tsx
+++
@@ -1,9 +0,0 @@
-import "./Welcome.css";
-export default function Welcome() {
-    return (
-        <section className="welcomeContainer" id="welcome">
-            <h1>Luís Daví</h1>
-            <h2>Desenvolvedor Front-end</h2>
-        </section>
-    );
-}
