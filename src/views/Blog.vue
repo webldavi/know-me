4e481364bce496cac62ba2f3393b607f72ee3338
Luis Davi <luisdavi.contato@gmail.com>
2023-09-24 15:31:36 -0300

    update blog section

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,12 +1,14 @@
 <template>
     <section class="blogContainer">
-        <template v-for="post in postsList" :key="post.id">
-            <Post :src="post['cover_image']" :url="post.url" :tags="post['tag_list']">
-                <template #title>{{ post.title }}</template>
-                <template #desc>{{ post.description }}</template>
-            </Post>
-            
-        </template>
+        <h1 class="text-2xl font-bold">Blog</h1>
+        <div>
+            <template v-for="post in postsList" :key="post.id">
+                <Post :src="post['cover_image']" :url="post.url" :tags="post['tag_list']">
+                    <template #title>{{ post.title }}</template>
+                    <template #desc>{{ post.description }}</template>
+                </Post>
+            </template>
+        </div>
     </section>
 </template>
 
@@ -16,15 +18,18 @@ import Post from '../components/Post.vue';
 
 const postsList = ref([])
 
-onBeforeMount(async() => {
-    await fetch('https://dev.to/api/articles?username=webdavi').then(res=>res.json()).then(res=>{
+onBeforeMount(async () => {
+    await fetch('https://dev.to/api/articles?username=webdavi').then(res => res.json()).then(res => {
         postsList.value = res
     })
 })
 </script>
 
 <style scoped>
-.blogContainer{
+.blogContainer {
+    @apply w-full h-max flex flex-col justify-center gap-12 items-center
+}
+.blogContainer > div{
     @apply w-full h-max flex flex-row flex-wrap justify-center gap-4
 }
 </style>
